4607b3af2f6cac5effbc1229ee7efde2566b0708
Paul Kabulu <pkabulu05@gmail.com>
2026-03-03 18:59:35 +0200

    created basic user methods in the db class

--- db.py
+++ db.py
@@ -5,50 +5,69 @@ import os
 load_dotenv()
 
 HOST = os.getenv("HOST")
-USER = os.getenv("USER")
+USER = os.getenv("DB_USER")
 PASSWORD = os.getenv("PASSWORD")
 DATABASE = os.getenv("DATABASE")
 PORT = os.getenv("PORT")
 
+print(USER)
+
 class DB:
     def __init__(self, host=HOST, user=USER, password=PASSWORD, database=DATABASE, port=PORT):
         self.connection = mysql.connector.connect(
-            host=os.getenv(host),
-            user=os.getenv(user),
-            password=os.getenv(password),
-            database=os.getenv(database)
+            host=host,
+            user=user,
+            password=password,
+            database=database,
+            port=port
         )
         self.cursor = self.connection.cursor()
 
-    def execute(self, query, params=None):
-        self.cursor.execute(query, params)
-        self.connection.commit()
+    # user related methods
+    def create_user(self, username, user_password):
+        self.cursor.execute("INSERT INTO Users (username, user_password) VALUES (%s, %s)", (username, user_password))
+        return self.connection.commit()
+    
+    def login_user(self, username:str, password:str):
+        """This method will handle user login and verify if the user exists, and if they do, verify whether the provided password matches the password in the db.
+        Returns: 
+            bool: Wether the info provided is true of false"""
 
-    def fetchall(self):
+        self.cursor.execute("SELECT username, user_password FROM Users WHERE username = %s", (username,))
+        user_info = self.cursor.fetchone()
+
+        if (username is None or not(username)):
+            return False
+        
+        if (username and (username == user_info[0] and password == user_info[1])):
+            return True
+        return False
+
+
+    def get_all_users(self):
+        self.cursor.execute("SELECT * FROM users")
         return self.cursor.fetchall()
+    
+    def get_user_by_id(self, user_id):
+        self.cursor.execute("SELECT * FROM Users WHERE user_id = %s", (user_id,))
+        return self.cursor.fetchone()
 
-    def close(self):
-        self.cursor.close()
-        self.connection.close()
+    def get_user_by_username(self, username):
+        self.cursor.execute("Select user_id, username FROM Users WHERE username = %s", (username,))
+        return self.cursor.fetchone()
+    
+    def get_group_members(self, group_id):
+        self.cursor.execute("SELECT * FROM GroupChatMembers WHERE group_id = %s", (group_id,))
+        return self.cursor.fetchmany()
+    
+    def delete_user(self, user_id, username):
+        self.cursor.execute("DELETE FROM USERS WHERE user_id = %s OR username = %s", (user_id, username))
+        return self.connection.commit()
+
 
 
 db = DB(host=HOST, user=USER, password=PASSWORD, port=PORT, database=DATABASE)
 
-#get all users
-
-def get_all_users():
-    db.execute("SELECT * FROM users")
-    return db.fetchall()
-
-
-def get_user_by_id(user_id):
-    db.execute("SELECT * FROM Users WHERE user_id = %d", (user_id))
-    return db.fetchall()
-
-def create_user(username, user_password):
-    db.execute("INSERT INTo Users (username, user_password) VALUES (%s, %s)", (username, user_password))
-
-
-
     
+
 
