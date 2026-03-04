e3ca3b59229c2fc0362b5d244518e80c977319d8
Paul Kabulu <pkabulu05@gmail.com>
2026-03-04 16:23:20 +0200

    added blob class to help with media or file storage and added method to get people contacted

--- db.py
+++ db.py
@@ -1,3 +1,5 @@
+from fileinput import filename
+
 from dotenv import load_dotenv
 import mysql.connector
 import os
@@ -9,6 +11,15 @@ USER = os.getenv("DB_USER")
 PASSWORD = os.getenv("PASSWORD")
 DATABASE = os.getenv("DATABASE")
 PORT = os.getenv("PORT")
+
+class Blob:
+    def __init__(self, file_path):
+        self.file_path = file_path
+    
+    def convert_to_binary_data(self):
+        with open(self.file_path, 'rb') as file:
+            binary_data = file.read()
+        return binary_data
 
 class DB:
     def __init__(self, host=HOST, user=USER, password=PASSWORD, database=DATABASE, port=PORT):
@@ -61,26 +72,39 @@ class DB:
     def delete_user(self, user_id, username):
         self.cursor.execute("DELETE FROM Users WHERE user_id = %s OR username = %s", (user_id, username))
         return self.connection.commit()
+    
+    def get_logged_status(self, user_id):
+        # Check whether someone is logged in
+        self.cursor.execute("SELECT logged_in from Users WHERE user_id = %s", (user_id,))
+        return self.cursor.fetchone()
 
     # Message related methods
 
-    def store_private_message(self, sender_id, receiver_id, message_text, media=None):
-        if (not (media and message_text)):
-                raise ValueError("Either message text or media must be provided.")
+    def store_private_message(self, sender_id, receiver_id, message_text, blob:Blob=None):
+        if (not (blob and message_text)):
+                raise ValueError("Either message text or file must be provided.")
         self.cursor.execute("INSERT INTO PrivateMessages (sender_id, receiver_id, message_text) VALUES (%s, %s, %s)", (sender_id, receiver_id, message_text))
-        if (media):
-            self.cursor.execute("INSERT INTO PrivateMessages (media) VALUES (%s)", (media,))
+        if (blob):
+            self.cursor.execute("INSERT INTO PrivateMessages (media) VALUES (%s)", (blob,))
         return self.connection.commit()
 
     def get_private_messages(self, sender_id, reciever_id):
         # this method will get private messages between two users, sender and reciever
-        self.cursor.execute("SELECT message_text, sent_at FROM PrivateMessages WHERE sender_id = %s OR sender_id=%s", (sender_id, reciever_id))
+        self.cursor.execute("SELECT message_text, sent_at, media FROM PrivateMessages WHERE sender_id = %s OR sender_id=%s", (sender_id, reciever_id))
         return self.cursor.fetchall()
+    
+    def get_contacts(self, user_id):
+        # this method will get all the contacts of a user, which are the users they have sent messages to or received messages from
+        self.cursor.execute("SELECT DISTINCT sender_id FROM PrivateMessages WHERE receiver_id = %s", (user_id,))
+        sent_contacts = self.cursor.fetchall()
+        self.cursor.execute("SELECT DISTINCT receiver_id FROM PrivateMessages WHERE sender_id = %s", (user_id,))
+        received_contacts = self.cursor.fetchall()
+        return set(sent_contacts + received_contacts)
 
 
 
 db = DB(host=HOST, user=USER, password=PASSWORD, port=PORT, database=DATABASE)
 
-print(db.get_all_users())
+print(db.get_contacts(1))
 
 
